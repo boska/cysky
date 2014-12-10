e2b0dddf51ed25a0fba590163e2baf3122e5c36b
Boska Lee <boska.lee@eztable.com>
2014-12-10 14:13:15 +0800

    remove comment

--- sky.rb
+++ sky.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 require 'onesky'
 require 'json'
 require 'colorize'
@@ -22,10 +23,18 @@ def get_file(project,filename,strings_location,locale)
 	files =  Dir.glob("#{strings_location}/**/#{filename}")
 	target = files.select {| p | p.include? "#{locale}.lproj"}.first if files
 	response = project.export_translation(source_file_name: filename, locale: locale) if target
-	File.open(target, 'w') { |file| file.write(response)} if target
+	File.open(target, 'w') { |file| file.write(clean_file(response))} if target && response
 	puts "Sync #{filename}@#{project.project_id}".green if target
 	puts "Do not exist #{filename}@#{project.project_id} in local".red if !target
 
+end
+def clean_file(response)
+	response if !response
+	response = response.delete!("\n")
+	response = response.gsub(/\/\*(.|[\r\n])*?\*\//,'')
+	response = response.gsub(/\/\/.\w+/,'')
+	response = response.gsub(/";"/,"\";\n\"")
+	response
 end
 @config.projects.each do |project_id,path| 
 	@config.locales.each do |locale| 
