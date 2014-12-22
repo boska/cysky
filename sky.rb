d335689e8194ca30403bd179ea2df861f1167a34
Boska Lee <boska.lee@eztable.com>
2014-12-22 13:48:15 +0800

    multi thread

--- sky.rb
+++ sky.rb
@@ -14,8 +14,8 @@ def sync_project(project_id,locale,project_path)
 	response = project.list_file
 	response = JSON.parse(response)
 	files = response['data'].map{|d| d["file_name"]}
-	puts "Files in #{project_id}"
-	files.each {|f| puts "#{f}".yellow }
+	#puts "Files in #{project_id}"
+	#files.each {|f| puts "#{f}".yellow }
 	files.each {|filename| get_file(project,filename,project_path,locale) }
 end
 
@@ -24,21 +24,30 @@ def get_file(project,filename,strings_location,locale)
 	target = files.select {| p | p.include? "#{locale}.lproj"}.first if files
 	response = project.export_translation(source_file_name: filename, locale: locale) if target
 	File.open(target, 'w') { |file| file.write(clean_file(response))} if target && response
-	puts "Sync #{filename}@#{project.project_id}".green if target
-	puts "Do not exist #{filename}@#{project.project_id} in local".red if !target
+	puts "Sync #{filename}@#{project.project_id}@#{locale}".green if target
+	puts "Do not exist #{filename}@#{project.project_id}@#{locale} in local".red if !target
 
 end
 def clean_file(response)
 	response if !response
 	response = response.delete!("\n")
 	response = response.gsub(/\/\*(.|[\r\n])*?\*\//,'')
-	response = response.gsub(/(?<!http:)\/\/\s\w+[^"]+/,'')
+	response = response.gsub(/(?<!http:)\/\/\s?\w+[^"]+/,'')
 	response = response.gsub(/";"/,"\";\n\"")
 	response
 end
+threads = []
+
 @config.projects.each do |project_id,path| 
 	@config.locales.each do |locale| 
-	puts "sync #{locale} @ #{project_id} @ #{path}".red
-	sync_project(project_id,locale,path)
+	threads << Thread.new do
+		puts "sync #{locale} @ #{project_id} @ #{path}".red
+		sync_project(project_id,locale,path)
+	 end
+	end
 end
+
+threads.each do |thread|
+    thread.join
 end
+puts "DONE!"
